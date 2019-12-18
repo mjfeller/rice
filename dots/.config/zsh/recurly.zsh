16d066432a2652130464d17db6434c1e4e4aea2d
Mark Feller <mfeller@recurly.com>
2019-12-18 13:49:50 -0700

    move recurly aliases
    Signed-off-by: Mark Feller <mfeller@recurly.com>

--- dots/.config/zsh/recurly.zsh
+++ dots/.config/zsh/recurly.zsh
@@ -4,3 +4,7 @@ rcmd() { if [[ $1 == "get" ]]; then shift; command rcmd list $@; else command rc
 
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
 export PATH="$PATH:$HOME/.rvm/bin"
+
+# Recurly
+alias rl="cd $GOPATH/src/github.com/recurly"
+alias grcmd="$GOPATH/bin/rcmd"
