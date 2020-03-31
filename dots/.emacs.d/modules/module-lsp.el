332806b23ab467253bc24e4fc4b6b458ae46a7c0
Mark Feller <mfeller@recurly.com>
2020-03-30 22:43:46 -0600

    major cleanup
    Signed-off-by: Mark Feller <mfeller@recurly.com>

--- dots/.emacs.d/modules/module-lsp.el
+++
@@ -1,9 +0,0 @@
-(use-package lsp-mode
-  :commands lsp)
-
-(use-package lsp-ui :commands lsp-ui-mode)
-(use-package company-lsp :commands company-lsp)
-
-
-(add-hook 'go-mode-hook #'lsp)
-(setq lsp-eldoc-render-all nil)
--- dots/.emacs.d/modules/module-markdown.el
+++
@@ -1,8 +0,0 @@
-(use-package markdown-mode)
-
-(use-package gh-md
-  :disabled)
-
-(provide 'module-markdown)
-
-;;; module-markdown.el ends here
